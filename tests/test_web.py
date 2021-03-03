981a98d5bf88a79baebbb7c26ad8cb7444cc2ba1
kevgray83 <75575265+kevgray83@users.noreply.github.com>
2021-03-03 21:55:28 +0000

    Started working on config file

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,14 +1,40 @@
 import pytest
-
+import json
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
-from selenium.webdriver import Chrome
+from selenium.webdriver import Chrome, Firefox
+
+
+@pytest.fixture(scope='session')
+def config():
+    with open('tests/config.json') as config_file:
+        data = json.load(config_file)
+    return data
+
+@pytest.fixture(scope='session')
+def config_browser(config):
+  if 'browser' not in config:
+    raise Exception('The config file does not contain "browser"')
+  elif config['browser'] not in ['chrome', 'firefox']:
+    raise Exception(f'"{config["browser"]}" is not a supported browser')
+  return config['browser']
+
+@pytest.fixture(scope='session')
+def config_wait_time(config):
+  return config['wait_time'] if 'wait_time' in config else 10
 
 @pytest.fixture
-def browser():
-    driver = Chrome()
-    driver.implicitly_wait(10)
+def browser(config_browser, config_wait_time):
+    if config_browser == 'chrome':
+        driver = Chrome()
+    elif config_browser =='firefox':
+        driver = Firefox()
+    else:
+        raise Exception(f'"{config["browser"]}" is not a supported browser')
+
+
+    driver.implicitly_wait(config_wait_time)
     yield driver
     driver.quit()
 
